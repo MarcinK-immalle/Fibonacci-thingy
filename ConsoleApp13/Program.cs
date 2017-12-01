31f2659d907bb48c5594414d4f1aa845dfd754df
marcin.kostulski <marcin.kostulski@B209-13>
2017-12-01 11:56:06 +0100

    sum things

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApp13
 {
     class Program
     {
+        static void printFib(int n)
+        {
+            
+            UInt64 get1 = 1;
+            UInt64 get2 = 1;
+            UInt64 som = 0;
+
+            for (int i = 0 ; i <= n ; i++)
+            {
+                som = get1 + get2;
+                get1 = get2;
+                get2 = som;
+                
+                Console.Write("{0}, ",som);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Gef het gewenste lengte : ");
+            int invoer = Convert.ToInt32(Console.ReadLine());
+            printFib(invoer);
         }
     }
 }
